d7342ad4358ddf6e1426a774ecafa9bae761b7a5
Egor4ikEgor4ikEgorik <prodolzitprodolzit@gmail.com>
2023-06-07 18:47:33 +0300

    Первая и начальная версия

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+print ("ПРОГРАММА ПО ПОГОДЕ ЗАПУЩЕНА!")
+params = {
+    "Mmn" : "",
+    "lang": "ru"
+
+}
+gorod=input('ВВЕДИТЕ ВАШ ГОРОД ')
+url = f"https://wttr.in/{gorod}"
+response = requests.get(url, params=params)
+
+response.raise_for_status()
+saratovy=["саратов","САРАТОВ","Саратов"]
+if gorod in saratovy :
+    print ("чел. из него нельзя выбраться.....")
+else:
+    print(response.text)
+
+
+
+
+
+
